87dfaa0425b253d6228d5d34eb9c557e4ea6077e
Alexander Dolzhenko <raskalovalexander@gmail.com>
2022-05-28 21:05:46 +0300

    fix: fixed menu styles when navigating

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
+  const { pathname } = useLocation();
   return (
     <header className="header page__header">
       <div className="header__container">
         <nav className="header__menu">
           <ul className="header__menu-list">
-            <li className="header__menu-item">
+            <li
+              className={`header__menu-item ${
+                pathname === "/cats" ? "header__menu-item_active" : ""
+              }`}
+            >
               <Link className="header__menu-link" to="/cats">
                 Все котики
               </Link>
             </li>
-            <li className="header__menu-item">
+            <li
+              className={`header__menu-item ${
+                pathname === "/saved-cats" ? "header__menu-item_active" : ""
+              }`}
+            >
               <Link className="header__menu-link" to="/saved-cats">
                 Любимые котики
               </Link>
