f84affb40a306e9f9c1b3fc6af4f7772161699b0
Alexander Dolzhenko <raskalovalexander@gmail.com>
2022-05-28 21:08:21 +0300

    refactor: clear code

--- src/components/CatsCard/CatsCard.jsx
+++ src/components/CatsCard/CatsCard.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./CatsCard.css";
 import cat from "../../images/cat.jpg";
-import like from "../../images/like.svg";
 
 const CatsCard = () => {
   return (
     <div className="cats-card">
       <img src={cat} alt="" className="cats-card__image" />
-      <button className="cats-card__btn">
-        {/* <img src={like} alt="Кнопка лайка" className="cats-card__btn-like" /> */}
-      </button>
+      <button className="cats-card__btn" />
     </div>
   );
 };
